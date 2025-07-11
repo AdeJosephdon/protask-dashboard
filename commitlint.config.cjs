036a96de2eccf2742e67f96036175d11a28544e9
Adegboyega Joseph <adegboyegajosephdon@gmail.com>
2025-07-12 00:04:22 +0100

    ci: fix linting error due to missing plugin

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-min-length': [2, 'always', 20],
-    'header-case': [2, 'always', 'sentence-case'],
+    'header-case': [2, 'never', 'sentence-case'],
     'subject-full-stop': [0],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
